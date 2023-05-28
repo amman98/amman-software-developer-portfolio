48307284d323d90ca02263cbfa5a3d3df6a0207c
Amman Nega <ammannega@gmail.com>
2023-05-28 01:02:05 -0700

    added header with links to each section

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react';
+import Navigation from '../Navigation';
+import './style.css';
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <h1>Amman Nega</h1>
+            <Navigation />
+        </div>
+    );
+}
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,16 @@
+import React,{useState} from 'react';
+import { Link, useLocation } from 'react-router-dom'
+import './style.css';
+
+export default function Navigation(props) {
+    const location = useLocation();
+
+    return (
+        <nav className='nav'>
+            <Link to='/' className={`nav-link ${location.pathname === '/' ? 'active' : 'inactive'}`}>About Me</Link>
+            <Link to='/portfolio' className={`nav-link ${location.pathname === '/portfolio' ? 'active' : 'inactive'}`}>Portfolio</Link>
+            <Link to='/contact' className={`nav-link ${location.pathname === '/contact' ? 'active' : 'inactive'}`}>Contact</Link>
+            <Link to='/resume' className={`nav-link ${location.pathname === '/resume' ? 'active' : 'inactive'}`}>Resume</Link>  
+        </nav>
+    );
+}
