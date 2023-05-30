ce60a44a2810deefe766afa57896041c74659006
Amman Nega <ammannega@gmail.com>
2023-05-30 02:14:26 -0700

    added four new projects to portfolio page

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Project from '../../components/Project';
 import biteBuddies from '../../images/Bite-Buddies.jpeg';
+import randomWordCalendar from '../../images/Random-Word-Calendar.jpeg';
+import techBlog from '../../images/Tech-Blog.jpeg';
+import wrestlingQuiz from '../../images/Wrestling-Quiz.png';
+
 import './style.css';
 
 export default function Portfolio() {
@@ -9,6 +13,9 @@ export default function Portfolio() {
             <h2>Portfolio</h2>
             <div className='projects'>
                 <Project card={biteBuddies} name={"Bite Buddies"} deploy={"https://bite-buddies.herokuapp.com/"} repo={"https://github.com/bear-muna/bite-buddies"} />
+                <Project card={randomWordCalendar} name={"Random Word Calendar"} deploy={"https://amman98.github.io/Random-Word-Calendar/"} repo={"https://github.com/amman98/Random-Word-Calendar"} />
+                <Project card={techBlog} name={"Tech Blog"} deploy={"https://agile-wildwood-77347.herokuapp.com/"} repo={"https://github.com/amman98/Tech-Blog"} />
+                <Project card={wrestlingQuiz} name={"Pro Wrestling Quiz"} deploy={"https://amman98.github.io/Pro-Wrestling-Quiz/"} repo={"https://github.com/amman98/Pro-Wrestling-Quiz"} />
             </div>
         </div>
     );
